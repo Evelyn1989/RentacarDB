d08c97cca378f1a51843eba63cbc757fcc33f8a7
Evelyn Johana Fallas Lopez <efallasl@edu.uc.ac.cr>
2026-08-05 22:26:31 -0600

    inicio sesion

--- Formularios/frmLogin.cs.Designer.cs
+++ Formularios/frmLogin.cs.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RentacarDB
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            ññ = new Label();
+            label2 = new Label();
+            txtUsuario = new TextBox();
+            txtContrasena = new TextBox();
+            btnIngresar = new Button();
+            btnSalir = new Button();
+            SuspendLayout();
+            // 
+            // ññ
+            // 
+            ññ.AutoSize = true;
+            ññ.Location = new Point(36, 31);
+            ññ.Name = "ññ";
+            ññ.Size = new Size(47, 15);
+            ññ.TabIndex = 0;
+            ññ.Text = "Usuario";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 110);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(129, 31);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(114, 23);
+            txtUsuario.TabIndex = 2;
+            // 
+            // txtContrasena
+            // 
+            txtContrasena.Location = new Point(129, 102);
+            txtContrasena.Name = "txtContrasena";
+            txtContrasena.Size = new Size(114, 23);
+            txtContrasena.TabIndex = 3;
+            txtContrasena.UseSystemPasswordChar = true;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(36, 179);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(75, 23);
+            btnIngresar.TabIndex = 4;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(183, 179);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(75, 23);
+            btnSalir.TabIndex = 5;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSalir);
+            Controls.Add(btnIngresar);
+            Controls.Add(txtContrasena);
+            Controls.Add(txtUsuario);
+            Controls.Add(label2);
+            Controls.Add(ññ);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmLogin";
+            SizeGripStyle = SizeGripStyle.Show;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Inicio de Sesión";
+            Load += frmLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label ññ;
+        private Label label2;
+        private TextBox txtUsuario;
+        private TextBox txtContrasena;
+        private Button btnIngresar;
+        private Button btnSalir;
     }
 }
--- Formularios/frmLogin.cs.cs
+++ Formularios/frmLogin.cs.cs
@@ -1,10 +1,15 @@
 namespace RentacarDB
 {
-    public partial class Form1 : Form
+    public partial class frmLogin : Form
     {
-        public Form1()
+        public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
