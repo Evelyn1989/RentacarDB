a5180161ef217f4abab00810f8b7f576853a233c
RedDevilCR92 <129443494+RedDevilCR92@users.noreply.github.com>
2026-08-26 19:50:23 -0600

    Update Cliente.cs

--- Clases/Cliente.cs
+++ Clases/Cliente.cs
@@ -1,11 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-using System;
 
 namespace RentacarDB.Modelos
 {
@@ -62,4 +59,3 @@ namespace RentacarDB.Modelos
         public string NombreCompleto => $"{Nombre} {Apellido} {SegundoApellido}".Trim();
     }
 }
-
