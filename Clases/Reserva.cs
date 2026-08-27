404a120b24a20b604e89003837db529d45be5e8d
RedDevilCR92 <129443494+RedDevilCR92@users.noreply.github.com>
2026-08-26 19:50:29 -0600

    Update Reserva.cs

--- Clases/Reserva.cs
+++ Clases/Reserva.cs
@@ -1,10 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-using System;
 
 namespace RentacarDB.Modelos
 {
@@ -41,7 +39,6 @@ namespace RentacarDB.Modelos
             IdSucursalDevolucion = idSucursalDevolucion;
         }
 
-    
         public int DiasReserva => (FechaFin - FechaInicio).Days;
     }
 }
